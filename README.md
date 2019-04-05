7eba7eab1eb890b426fabed7380b048534ef2dde
Jake Nagel <jdnagel1@gmail.com>
2019-04-05 16:59:17 -0700

    completely redo all logic and finalize project for submittal

---
+++ README.md
@@ -0,0 +1,70 @@
+# _Beep Boop_
+
+#### _This web application takes a number from a user and returns a range of numbers from 0 to the user inputted number with the following exceptions:
+
+Numbers that contain a 1: all digits are replaced (all digits) with "Beep!"
+Numbers that contain a 2: all digits are replaced (all digits) with "Boop!"
+Numbers that contain a 3: all digits are replaced (all digits) with "I'm sorry, Dave. I'm afraid I can't do that."
+These exceptions are written from least to most important. The first exception should apply unless the second exception does, and the same with the second and third. So, for example, in your finished program,
+
+The number 13 should be replaced with "I'm sorry, Dave. I'm afraid I can't do that."
+The number 21 should be replaced with "Boop".
+The number 32 should be replaced with "I'm sorry, Dave. I'm afraid I can't do that."
+The user is also able to enter a new number and see new results over and over again._
+
+#### By _Jake Nagel_
+
+
+## Setup/Installation Requirements
+
+* _Open Site in Google Chrome_
+* _Enter Number_
+* _View Results_
+
+## Known Bugs
+
+_No Known Bugs_
+
+## Specs
+
+Spec: The program returns a range of numbers from 0 to the users inputted number
+Input: "4"
+Output: "0, 1, 2, 3, 4"
+
+input: create number variable for all user input
+output: false
+
+input: make a loop that stores user input into array
+output: 1,2,3,4,5
+
+input: replace all numbers that contain 1
+output: beep
+
+input: replace all numbers that contain 2
+output: boop
+
+input: replace all numbers that contain 3
+output: "I'm sorry, Dave. I'm afraid I can't do that.!"
+
+
+## Support and contact details
+
+_jdnagel1@gmail.com_
+
+## Technologies Used
+
+_HTML_
+_CSS_
+_Bootstrap_
+_Javascript_
+_jQuery_
+
+### License
+
+Copyright 2019 Jake Nagel
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
--- js/scripts.js
+++ js/scripts.js
@@ -1,133 +1,40 @@
 $(document).ready(function() {
-  $("form").submit(function(event) {
-    event.preventDefault();
-    // User Interface Logic
-    // var userInput = $("input#input").val();
-    // var translatedString = translateSentenceToPigLatin(userInput);
-    $('.result').show();
-    var userInput = parseInt($("#input").val());
-    $('#original').text(userInput);
-    var translatedString = convertToBeeps(userInput);
-    $('#beepBoopTranslation').text(translatedString);
-  });
-  // console.log(isVowel(""));
+  $('#beepForm').submit(function(event){
+      event.preventDefault();
+      var mintGum = $('input#number').val();
+      stringCheese = [];
+      pushIt(mintGum);
+      checkMate();
+      $("ul#answer").empty();
+      answer();
+      console.log(stringCheese);
+    });
+
 });
 
+var stringCheese = [];
+function pushIt(num){
+  for(var i = 0; i <= num; i++){
+    stringCheese.push("" + i);
+  };
+};
 
-// business logic
+function checkMate(){
+  var holdIt = "";
+  for(var x = 0; x < stringCheese.length; x++){
+    holdIt = stringCheese[x];
+    if(holdIt.includes("3")){
+      stringCheese[x] = "I'm sorry, Dave. I'm afraid i can't do that";
+    }else if(holdIt.includes("2")){
+      stringCheese[x] = "Boop!";
+    }else if(holdIt.includes("1")){
+      stringCheese[x] = "Beep!";
+    };
+  };
+};
 
-function convertToBeeps(num) {
- var beepToBoop = ["I'm sorry, Dave. I'm afraid I can't do that",'Boop','Beep'];
- var nums = [3,2,1]
-
-  var neo = "";
-
-  for (i = 0; i < beepToBoop.length; i++) {
-    var key = beepToBoop[i];
-    while (num >= nums[i]) {
-      neo += key;
-      num -= nums[i];
-      console.log("Num is decreased: ", num)
-    }
-  }
- return neo;
-}
-
-// $(document).ready(function() {
-//   $("form").submit(function(event) {
-//     event.preventDefault();
-//     // User Interface Logic
-//     var inputOne = $("input#input").val();
-//     var inputTwo = numberToBeep(userInput);
-//     $('.result').show();
-//     $('#original').text(userInput);
-//     $('#beepBoopTranslation').text(translatedString);
-//   });
-//
-//
-// // // Business logic
-//   function convertToRoman(num) {
-//    var romanToNum = ['M','CM','D','CD','C','XC','L','XL','X','IX',"Im sorry Dave. I'm afraid I can't do that",'Boop','Beep'];
-//    var nums = [1000,900,500,400,100,90,50,40,5,4,3,2,1]
-//    // var nums = [1000,900,500,400,100,90,50,40,10,9,5,4,1]
-//
-//     var roman = "";
-//
-//     for (i = 0; i < romanToNum.length; i++) {
-//       var key = romanToNum[i];
-//       // console.log("key: ", key)
-//       // console.log("value: ", romanToNum[key]);
-//       while (num >= nums[i]) {
-//         roman += key;
-//         num -= nums[i];
-//         console.log("Num is decreased: ", num)
-//       }
-//     }
-//    return roman;
-//   }
-
-
-// // Business logic
-// function isVowel(character) {
-//   if ((character === 'a') || (character === 'e') || (character === 'i') || (character === 'o') || (character === 'u') || (character === 'y')) {
-//     return true;
-//   } else {
-//     return false;
-//   }
-// }
-//
-// function findFirstVowel(word) {
-//   var wordArray = word.split("");
-//   for (var i = 0; i <= wordArray.length - 1; i++) {
-//     var letter = wordArray[i].toLowerCase();
-//     if (isVowel(letter)){
-//       return i;
-//     }
-//   }
-// }
-//
-// function addVowelSuffix(word) {
-//   var newWord = word + 'way';
-//   return newWord.toLowerCase();
-// }
-//
-// function translateConsonant(word) {
-//   var vowelLocation = findFirstVowel(word);
-//   var startOfWord = word.slice(0, (vowelLocation));
-//   var endOfWord = word.slice(vowelLocation);
-//   var newWord = endOfWord + startOfWord + "ay";
-//   return newWord.toLowerCase();
-// }
-//
-// function translateSentenceToPigLatin(sentence) {
-//   var translatedSentence = "";
-//   var sentenceArray = sentence.split(" ");
-//   for (var i = 0; i <= sentenceArray.length - 1; i++) {
-//     var word = sentenceArray[i];
-//     if (findFirstVowel(word) === 0){
-//       translatedSentence += addVowelSuffix(word) + " ";
-//     } else if(findFirstVowel(word) > 0){
-//       translatedSentence += translateConsonant(word) + " ";
-//     } else {
-//       console.log("error in word");
-//     }
-//   }
-//   translatedSentence = translatedSentence.charAt(0).toUpperCase() + translatedSentence.slice(1);
-//   return translatedSentence;
-// }
-//
-//
-// // /^+$/gi
-//
-// $(document).ready(function() {
-//   $("form").submit(function(event) {
-//     event.preventDefault();
-//     // User Interface Logic
-//     var userInput = $("input#input").val();
-//     var translatedString = translateSentenceToPigLatin(userInput);
-//     $('.result').show();
-//     $('#original').text(userInput);
-//     $('#pigLatinTranslation').text(translatedString);
-//   });
-//   // console.log(isVowel(""));
-// });
+function answer(){
+  for(var y = 0; y < stringCheese.length; y++){
+    $('ul#answer').append("<li>" + stringCheese[y] + "</li>");
+  };
+};
